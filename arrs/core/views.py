e8495ce852c9c0d43bc4484ce4470d3ce5d099cb
Boling DevOps <geekish2428@gmail.com>
2019-09-08 05:04:56 +0100

    Sussing out core/view handlers

--- arrs/core/views.py
+++ arrs/core/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 def index(request):
     return HttpResponse("Hello world. This is the ARRS index view.")
+
+def dashboard(request):
+    pass
+
+def seeRounds(request):
+    pass
+
+def seeReports(request):
+    pass
+
+def addRound(request):
+    pass
+
+def addReport(request):
+    pass
