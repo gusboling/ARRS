b59cb31864283ae29b0b84dfd5f5ac4473003e2f
Shady Prophet <geekish2428@gmail.com>
2019-10-28 18:44:22 -0600

    Added bootstrap resources & static webpages for template skeleton

--- arrs/core/views.py
+++ arrs/core/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
     return HttpResponse("Hello world. This is the ARRS index view.")
 
 def dashboard(request):
-    pass
+    template = loader.get_template(templates/dashboard.html)
+    context = {}
+    return HttpResponse(template.render(context, request))
 
 def seeRounds(request):
     pass
