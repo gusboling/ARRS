6fc3c668ad0ca2727d000f397ff6bc9978a470dd
Shady Prophet <geekish2428@gmail.com>
2019-10-28 21:34:22 -0600

    Added data-entry page for rounds, associated URL routing

--- arrs/core/urls.py
+++ arrs/core/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+#TODO eliminate index page/url routing?
 urlpatterns = [
     path('', views.index, name='index'),
-    path('dashboard', views.dashboard, name="dashboard")
+    path('dashboard', views.dashboard, name="dashboard"),
+    path('addRound', views.addRound, name="addRound")
 ]
--- arrs/core/views.py
+++ arrs/core/views.py
@@ -5,8 +5,15 @@ from django.template import loader
 def index(request):
     return HttpResponse("Hello world. This is the ARRS index view.")
 
+def login(request):
+    #TODO figure out if this is correct way to implement a secure login.
+    template = loader.get_template("login.html")
+    context = {}
+    return HttpResponse(template.render(context, request))
+
 def dashboard(request):
     template = loader.get_template("dashboard.html")
+    #TODO populate this with relevant information; see dashbooard.html TODOs
     context = {}
     return HttpResponse(template.render(context, request))
 
@@ -17,7 +24,10 @@ def seeReports(request):
     pass
 
 def addRound(request):
-    pass
+    template = loader.get_template("addRound.html")
+    #TODO populate this with relevant information; see addRound.html TODOs
+    context = {}
+    return HttpResponse(template.render(context, request))
 
 def addReport(request):
     pass
