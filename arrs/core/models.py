4485669804605a7512bbc0913612061abcb17a6e
Shady Prophet <geekish2428@gmail.com>
2019-10-31 20:25:02 -0600

    Initial attempt at a top-dozen function

--- arrs/core/models.py
+++ arrs/core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from datetime import date
 
+import operator
 
 #Model representing a tournament
 #NOTE: has a many-to-one relationship with Round, Many-to-many with Comp
@@ -20,35 +21,42 @@ class Comp(models.Model):
     event = models.CharField(max_length=100)
     varsity = models.BooleanField(default=False)
 
-    #Returns name of the competitor
+    #ACCESOR METHODS
     def getName(self):
         return self.name
 
-    #Returns (name?) of all rounds associated with comp
-    def getRounds(self):
-        comprounds = {"affirmative": self.affcomp.all(), "negative": self.negcomp.all()}
-        return comprounds
-        
+    def getEvent(self):
+        return self.event
 
-    #Iterates over self.getRounds() and returns the number of rounds where comp won.
+    def getLevel(self):
+        return ("Novice", "Varsity")[self.varsity]
+
+    def getRounds(self):
+        comprounds = {"affrounds": self.affcomp.all(), "negrounds": self.negcomp.all()}
+        return comprounds
+
+    #STAT METHODS
+    #DESC: Returns integer representing total number of rounds won.
     def getWinCount(self):
         win_count = 0
         rnds = self.getRounds()
-        for rnd in rnds["affirmative"]: 
+        for rnd in rnds["affirmative"]:
             if rnd.getWinner() == self.getName(): win_count += 1
-        for rnd in rnds["negative"]: 
+        for rnd in rnds["negative"]:
             if rnd.getWinner() == self.getName(): win_count += 1
         return win_count
 
-    #Returns value of len(self.getRounds()) minus self.getWinCount()
+
+    #DESC: Returns integer representing total number of rounds lost.
     def getLossCount(self):
         rnds = self.getRounds()
-        totalRounds = len(rnds["affirmative"]) + len(rnds["negative"])
-        return totalRounds - self.getWinCount()
+        total_rounds = len(rnds["affrounds"]) + len(rnds["negrounds"])
+        return total_rounds - self.getWinCount()
 
+    #TOSTRING METHOD
     def __str__(self):
-        vString = ("N", "V")[self.varsity]
-        return self.name + " (" + vString + " " + self.event + ") " + str(self.getWinCount()) + "-" + str(self.getLossCount()) 
+        return self.getName() + " (" + self.getLevel() + " " + self.getEvent() + ")
+
 
 #Model representing a single debate round
 #NOTE: has a many-to-one relationship with Comp model
@@ -89,3 +97,11 @@ class Round(models.Model):
 
     def __str__(self):
         return str(self.tournament) + " " + self.getAff() + " v. " + self.getNeg() + " (" + self.type + ")"
+
+def getVarsityPolicyDozen():
+    vpol_comps = Comp.objects.filter(event="Policy").filter(varsity=True)
+    vpol_dict = {}
+    for vc in vpol_comps:
+        vpol_dict[vc.getName()] = vc.getWinCount()
+    ranked_comps = sorted(vpol_dict.items(), key=operator.itemgetter(1))
+    print(ranked_comps)
