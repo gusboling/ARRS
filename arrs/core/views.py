23fc821fc1000a971796e9bc522afd04466720a2
Shady Prophet <geekish2428@gmail.com>
2019-11-07 21:16:16 -0700

    Added login_required decorator to view methods

--- arrs/core/views.py
+++ arrs/core/views.py
@@ -1,18 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, login_required
 
 import core.models
 
-def auth(request):
-    pass
-
-def index(request):
-    template = loader.get_template("login.html")
-    context = {}
-    return HttpResponse(template.render(context, request))
-
+@login_required
 def dashboard(request):
     template = loader.get_template("dashboard.html")
     #TODO populate this with relevant information; see dashbooard.html TODOs
@@ -21,25 +14,29 @@ def dashboard(request):
         "NPO": core.models.getEventTopComps("NPO", 6)
     }
     context = {
-        "vpo_top":event_top_six["VPO"]
+        "vpo_top":event_top_six["VPO"],
+        "npo_top":event_top_six["NPO"]
     }
     return HttpResponse(template.render(context, request))
 
+@login_required
 def viewRounds(request):
     template = loader.get_template("records.html")
     context = {}
     return HttpResponse(template.render(context, request))
 
+@login_required
 def addRound(request):
     template = loader.get_template("addRound.html")
     #TODO populate this with relevant information; see addRound.html TODOs
     context = {}
     return HttpResponse(template.render(context, request))
 
-
 #STAGE II
+@login_required
 def viewReports(request):
     pass
 
+@login_required
 def addReport(request):
     pass
