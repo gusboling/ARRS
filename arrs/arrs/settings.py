af3563f515f71d87af11912323de0d0a2ed8b340
Shady Prophet <geekish2428@gmail.com>
2019-11-09 15:58:54 -0700

    Fixed login/logout redirects and implemented Bootstrap login page

--- arrs/arrs/settings.py
+++ arrs/arrs/settings.py
@@ -61,7 +61,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'arrs.urls'
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/core/dashboard'
+LOGOUT_REDIRECT_URL = '/accounts/login'
 
 TEMPLATES = [
     {
@@ -122,13 +123,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
