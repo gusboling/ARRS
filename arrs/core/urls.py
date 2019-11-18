c04e067cd3d6939d46c6d44ad03c221a30835cb6
Shady Prophet <geekish2428@gmail.com>
2019-11-17 20:48:39 -0700

    Added view competitors stuff to dashboard

--- arrs/core/urls.py
+++ arrs/core/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('dashboard', views.dashboard, name="dashboard"),
     path('addRound', views.addRound, name="addRound"),
     path('viewRounds', views.viewRounds, name="viewRound"),
+    path('viewCompetitors', views.viewCompetitors, name="viewCompetitors")
 ]
