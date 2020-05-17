9f4a799be3efb5a483da3656ef34c38c3b87474a
Boling DevOps <geekish2428@gmail.com>
2020-05-17 03:56:31 +0100

    Branched for data model refactor; updated core/models.py

--- arrs/core/models.py
+++ arrs/core/models.py
@@ -1,167 +1,99 @@
 from django.db import models
-from datetime import date
 
-import operator
-
-#Model representing a tournament
-#NOTE: has a many-to-one relationship with Round, Many-to-many with Comp
-class Tournament(models.Model):
-    location = models.CharField(max_length=100, null=False, default="Unofficial")
-    year = models.IntegerField(default=date.today().year)
-
-    def __str__(self):
-        return self.location + " " + str(self.year)
-
-
-#Model representing a single competitor/competitor-partnership
-#NOTE: has none-to-many relationship with Round model
-class Comp(models.Model):
-
-    name = models.CharField(max_length=50, unique=True)
-    #varsity = models.BooleanField(default=False)
-    onteam = models.BooleanField(default=False)
-
-    EVENT_CHOICES = [
-    ("VPO", "Varsity Policy"),
-    ("VLD", "Varsity Lincoln Douglas"),
-    ("VPF", "Varsity Public Forum"),
-    ("NPO", "Novice Policy"),
-    ("NLD", "Novice Lincoln Douglas"),
-    ("NPF", "Novice Public Forum")
-    ]
-    event = models.CharField(null=False, max_length=100, choices=EVENT_CHOICES, default="VPO")
-
-    #ACCESOR METHODS
-    def getName(self):
-        return self.name
-
-    def getEvent(self):
-        return self.event
-
-    def getLevel(self):
-        #return ("Novice", "Varsity")[self.varsity]
-        pass
-
-    def getRounds(self):
-        comprounds = {"affrounds": self.affcomp.all(), "negrounds": self.negcomp.all()}
-        return comprounds
-
-    #STAT METHODS
-    #DESC: Returns integer representing total number of rounds won.
-    def getWinCount(self):
-        win_count = 0
-        rnds = self.getRounds()
-        for rnd in rnds["affrounds"]:
-            if rnd.getWinner() == self.getName(): win_count += 1
-        for rnd in rnds["negrounds"]:
-            if rnd.getWinner() == self.getName(): win_count += 1
-        return win_count
-
-
-    #DESC: Returns integer representing total number of rounds lost.
-    def getLossCount(self):
-        rnds = self.getRounds()
-        total_rounds = len(rnds["affrounds"]) + len(rnds["negrounds"])
-        return total_rounds - self.getWinCount()
-
-    #TOSTRING METHOD
-    def __str__(self):
-        return self.getName() + " (" + self.getEvent() + ")"
-
-
-#Model representing a single debate round
-#NOTE: has a many-to-one relationship with Comp model
-#TODO: create subclasses for speech/debate rounds (PHASE II)
 class Round(models.Model):
-
-    tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE, null=True)
-    aff = models.ManyToManyField(Comp, related_name="affcomp")
-    neg = models.ManyToManyField(Comp, related_name="negcomp")
-
-    #Round Type Information
+    """A single debate round
+    Members:
+    --------
+    debater: str, required
+        character field corresponding to the name field of a Comp-object instance
+    opponent: str, required
+        character field containing the name of the opponent
+    bracket: str, required
+        3-character code representing round-type (PRE, OCT, QRT, SEM, FIN)
+    won: boolean, required
+        Indicates if the debater won the round
+    position: boolean, required
+        Indicates if the debater argued AFF
+    event:
+        3-character code representing round-event (VPO, VLD, VPF, NPO, NLD)
+    """
+    #Relational Fields
+    debater = models.CharField() #Make foreign key?
+    tournament = models.CharField() #Make foreign key?
+    
+    #Local fields
     ROUND_TYPES = [
         ("PRE", "Preliminary"),
-        ("TOC", "Triple Octafinal"),
-        ("DOC", "Double Octafinal"),
         ("OCT", "Octafinal"),
         ("QRT", "Quarterfinal"),
         ("SEM", "Semifinal"),
         ("FIN", "Final")
     ]
-    RESULTS = [("AFF", "Affirmative Win"), ("NEG", "Negative Win")]
-    type = models.CharField(null=False, max_length=3, choices=ROUND_TYPES, default="PRE")
-    result = models.CharField(null=False, max_length=3, choices=RESULTS, default="AFF")
 
-    #GET METHODS
-    def getAff(self):
-        return self.aff.all()[0].name
-    def getNeg(self):
-        return self.neg.all()[0].name
-    def getWinner(self):
-        if self.result == "AFF":
-            return self.getAff()
-        else:
-            return self.getNeg()
+    EVENT_TYPES = [
+        ("VPO", "Varsity Policy"),
+        ("VLD", "Varsity Lincoln-Douglas"),
+        ("VPF", "Public Forum"),
+        ("NPO", "Novice Policy"),
+        ("NLD", "Novice Lincoln-Douglas")
+    ]
+
+    opponent = models.CharField(max_length=100)
+    bracket = models.CharField(max_length=3, choices=ROUND_TYPES)
+    event = models.CharField(max_length=3, choices=EVENT_TYPES)
+    won = models.BooleanField(default=False)
+    position = models.BooleanField(default=False)
+
+    def get_debater(self):
+        return self.debater
+
+    def get_tournament(self):
+        return self.tournament
+
+    def get_braket(self):
+        return self.bracket
+
+    def get_won(self):
+        return self.won
+
+    def get_position(self):
+        return self.position
 
     def __str__(self):
-        return str(self.tournament) + " " + self.getAff() + " v. " + self.getNeg() + " (" + self.type + ")"
+        return str(self.debater) + "v." + str(self.opponent)
 
+class Comp(models.Model):
+    """A single team or competitor
+    Members:
+    --------
+    name: str, required, primary key
+        name of the debater/team
+    """
+    name = models.CharField(max_length=100, unique=True)
 
-#VIEW METHODS
-def getEventTopComps(tevent, cutoff):
-    top_comps = Comp.objects.filter(event=tevent).filter(onteam=True)
-    sorted_comps = []
-    names = []
-    for vc in top_comps:
-        sorted_comps.append((vc.getName(), vc.getWinCount()))
-    sorted_comps = sorted(sorted_comps, key=lambda tup: tup[1])
-    sorted_comps.reverse()
-    for sc in sorted_comps:
-        names.append(sc[0])
+    def get_name(self):
+        return self.name
 
-    return names[0:cutoff]
+    def __str__(self):
+        return str(self.name)
 
-def getCompWinDataSets(tevent, homeTeam=True):
-    resultComps = Comp.objects.filter(event=tevent).filter(onteam=homeTeam)
-    compTuples = {"names": [], "wins": []}
-    for rc in resultComps:
-        compTuples["names"].append(rc.getName())
-        compTuples["wins"].append(rc.getWinCount())
-    return compTuples
+class Tournament(models.Model):
+    """A single tournament
+    Members:
+    --------
+    location: str, required
+        Place where tournament took place (e.g. Bozeman, Helena, etc.)
+    year: str, required
+        Year of the tournament (2020, etc.)
+    """
+    location = models.CharField(max_length=100)
+    year = models.CharField(max_length=4)
+    
+    def get_location(self):
+        return self.location
 
-def getRounds(tevent):
-    resultRounds = Round.objects.all()
-    return resultRounds
+    def get_year(self):
+        return self.year
 
-#Awful hacky spaghetti; fix when a better person
-def getViewRounds(tevent):
-    rounds = getRounds(tevent)
-    result = []
-    for r in rounds:
-        roundData = {
-                "event": r.type,
-                "aff": r.getAff(),
-                "neg": r.getNeg(),
-                "result": r.getWinner()
-        }
-        result.append(roundData)
-    return result
-
-def getComps():
-    comps = Comp.objects.all()
-    result = []
-    for c in comps:
-        compObj = {
-                "name": c.getName(),
-                "event": c.getEvent(),
-                "winCount": c.getWinCount(),
-                "roundCount": len(c.getRounds())
-        }
-        if c.onteam:
-            compObj["school"] = "Bozeman High School"
-        else:
-            compObj["school"] = "Other"
-
-        result.append(compObj)
-    return result
-
+    def __str__(self):
+        return str(self.location) + " " + str(self.year)
